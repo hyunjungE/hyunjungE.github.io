e7e52f4de131e245a19ae13cf36f0c5dbc4681f7
hyunjungE <jjung9348@gmail.com>
2022-06-29 18:18:39 +0900

    지도 샘플 변경

--- js/story.js
+++ js/story.js
@@ -32,10 +32,9 @@ let MultiSwiper = new Swiper(".video_list", {
     },
   });
   /*---------------------------- */
-
 document.addEventListener('DOMContentLoaded', function() {
     
-var  mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+/*var  mapContainer = document.getElementById('map'), // 지도를 표시할 div 
 mapOption = { 
     center: new kakao.maps.LatLng(37.570769290372255,126.98181158018586), // 지도의 중심좌표
     level: 3 // 지도의 확대 레벨
@@ -59,5 +58,23 @@ function setZoomable(zoomable) {
     // 마우스 휠로 지도 확대,축소 가능여부를 설정합니다
     map.setZoomable(zoomable);    
 }
-setZoomable(false); 
+setZoomable(false); */
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(37.570769290372255,126.98181158018586), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(37.570769290372255,126.98181158018586); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
 });
